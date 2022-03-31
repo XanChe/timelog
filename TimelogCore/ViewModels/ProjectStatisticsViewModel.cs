a2fa2c6786f101f7609fee935279d18115711c13
XanCh <xandrmail@mail.ru>
2022-03-31 08:48:17 +0600

    правка модуля статистики

--- TimelogCore/ViewModels/ProjectStatisticsViewModel.cs
+++ TimelogCore/ViewModels/ProjectStatisticsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Timelog.Core.ViewModels
 {
     public class ProjectStatisticsViewModel: TotalStatisticsVewModel
     {
+        [Display(Name = "Проект")]
         public string ProjectName { get; set; }
         public long ProjectId { get; set; }
     }
